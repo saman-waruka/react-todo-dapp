77184ccb259f6acb76ff8b7839931ac1c2d0b69d
saman <saman_waruka@cmu.ac.th>
2022-05-06 17:23:32 +0700

    fix: cannot start when import web3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import Web3 from "web3";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    loadBlockchainData();
+  }, []);
+  const loadBlockchainData = async () => {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+    const network = await web3.eth.net.getNetworkType();
+    console.log(" network ", network);
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hello world</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +26,7 @@ function App() {
         >
           Learn React
         </a>
+        <button> connect </button>
       </header>
     </div>
   );
