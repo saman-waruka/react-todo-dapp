13b15c08fba02904aa6e55cff8cbdf96efb9cd04
saman-waruka <saman_waruka@cmu.ac.th>
2022-05-09 00:50:15 +0700

    fix: cannot use web3 in react by reverse to react-scripts@4.0.3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect , useState} from "react";
+import Web3 from "web3";
 
-function App() {
+import logo from "./logo.svg";
+import "./App.css";
+
+const App = () => {
+
+  const [account, setAccount] = useState('');
+  
+  useEffect(() => {
+    loadBlockchainData();
+  }, []);
+
+  const loadBlockchainData = async () => {
+    // const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+    const web3 = new Web3( "http://localhost:8545");
+    const network = await web3.eth.net.getNetworkType();
+    const accounts = await web3.eth.getAccounts();
+    setAccount(accounts[0])
+    console.log(" network ", network);
+    console.log(" account ", accounts);
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hello world</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +34,8 @@ function App() {
         >
           Learn React
         </a>
+        <p>Your account: { account}</p>
+        <button onClick={()=> loadBlockchainData()}> connect </button>
       </header>
     </div>
   );
