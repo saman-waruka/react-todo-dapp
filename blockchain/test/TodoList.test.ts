9edbda91c58c0684b032c4881d2b25567d657366
saman <saman_waruka@cmu.ac.th>
2022-05-10 03:52:24 +0700

    feat: add todolist blockchain

---
+++ blockchain/test/TodoList.test.ts
@@ -0,0 +1,62 @@
+/* eslint-disable no-unused-vars */
+import { expect } from "chai";
+import { assert } from "console";
+import { ethers } from "hardhat";
+import { TodoList } from "./../typechain";
+
+let todoList: TodoList;
+
+describe("TodoList", function () {
+  it("The contract is able to deploy", async function () {
+    const TodoList = await ethers.getContractFactory("TodoList");
+    todoList = await TodoList.deploy();
+    await todoList.deployed();
+    console.log("TodoList deployed to: ", todoList.address);
+    expect(todoList.address).not.equal(0x0);
+    expect(todoList.address).not.equal("");
+    expect(todoList.address).not.equal(null);
+    expect(todoList.address).not.equal(undefined);
+  });
+
+  it("create task", async () => {
+    const content = "Test create task";
+    const transaction = await todoList.createTask(content);
+    const task = await todoList.tasks(1);
+    const receipt = await transaction.wait();
+    // @ts-ignore
+    const eventResult = receipt.events[0].args as TodoList.Task;
+    expect(task.id.toNumber()).to.equal(1);
+    expect(task.content).to.equal(content);
+    expect(task.completed).to.equal(false);
+    expect(eventResult.id.toNumber()).to.equal(1);
+    expect(eventResult.content).to.equal(content);
+    expect(eventResult.completed).to.equal(false);
+  });
+
+  it("list task", async () => {
+    const taskCount = await (await todoList.taskCount()).toNumber();
+    expect(taskCount).to.equal(1);
+  });
+
+  it("toggle task completion", async () => {
+    const transaction = await todoList.toggleTaskComplete(1);
+    const task = await todoList.tasks(1);
+    const receipt = await transaction.wait();
+    // @ts-ignore
+    const eventResult = receipt.events[0].args as TodoList.Task;
+    expect(task.completed).to.equal(true);
+    expect(eventResult.id.toNumber()).to.equal(1);
+    expect(eventResult.completed).to.equal(true);
+  });
+
+  it("toggle task to incomplete", async () => {
+    const transaction = await todoList.toggleTaskComplete(1);
+    const task = await todoList.tasks(1);
+    const receipt = await transaction.wait();
+    // @ts-ignore
+    const eventResult = receipt.events[0].args as TodoList.Task;
+    expect(task.completed).to.equal(false);
+    expect(eventResult.id.toNumber()).to.equal(1);
+    expect(eventResult.completed).to.equal(false);
+  });
+});
