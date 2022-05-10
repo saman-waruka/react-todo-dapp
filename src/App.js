ddcdf4cfcde6d782865e817fb9c5d6abeb91b4ab
saman <saman_waruka@cmu.ac.th>
2022-05-10 11:12:11 +0700

    feat: detect change account & network event

--- src/App.js
+++ src/App.js
@@ -16,6 +16,18 @@ const App = () => {
     loadBlockchainData();
   }, []);
 
+  window.ethereum.on("accountsChanged", function (accounts) {
+    // Time to reload your interface with accounts[0]!
+    console.log("account changed ", accounts);
+    loadBlockchainData();
+  });
+
+  window.ethereum.on("networkChanged", function (networkId) {
+    // Time to reload your interface with the new networkId
+    console.log("network  changed ", networkId);
+    loadBlockchainData();
+  });
+
   const createTask = (content) => {
     setLoading(true);
     todoListContract.methods
@@ -24,6 +36,10 @@ const App = () => {
       .once("receipt", (receipt) => {
         console.log(" receipt ", receipt);
         setLoading(false);
+        const newTask = receipt.events.TaskCreated.returnValues;
+        setTasks([...tasks, newTask]);
+        setTaskCount(Number(taskCount) + 1);
+        // loadBlockchainData();
       });
   };
 
@@ -41,20 +57,24 @@ const App = () => {
   const loadBlockchainData = async () => {
     const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
     const network = await web3.eth.net.getNetworkType();
-    setNetwork(network);
+
     const accounts = await web3.eth.getAccounts();
-    setAccount(accounts[0]);
 
     const todoList = new web3.eth.Contract(TODO_LIST_ABI, TODO_LIST_ADDRESS);
-    setTodoListContract(todoList);
+
     const taskCount = await todoList.methods.taskCount().call();
-    setTaskCount(taskCount);
+
     const allTask = [];
     for (let i = 1; i <= taskCount; i++) {
       const thisTask = await todoList.methods.tasks(i).call();
-      console.log("thisTask ", thisTask);
+      // console.log("thisTask ", thisTask);
       allTask.push(thisTask);
     }
+    console.log("loadBlockchainData  ");
+    setNetwork(network);
+    setAccount(accounts[0]);
+    setTodoListContract(todoList);
+    setTaskCount(taskCount);
     setTasks(allTask);
   };
   return (
